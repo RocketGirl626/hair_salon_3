f3fc8692dfcc3ed180ca2e289425cdfa9f4cffbb
Jeanette Fairless <jfairless@spectrumhomeservices.com>
2015-05-08 10:09:22 -0700

    add the spec for find and save.

---
+++ Desktop/hair_salon/lib/stylist.rb
@@ -0,0 +1,23 @@
+class Stylist
+attr_reader(:name, :id)
+
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id).to_i
+  end
+
+  define_singleton_method(:all) do
+    returned_stylists = DB.exec("SELECT * FROM stylists;")
+    stylists = []
+    returned_stylists.each() do |stylist|
+      name = stylist.fetch('name')
+      id = stylist.fetch('id')
+      stylists.push(Stylist.new({:name => name, :id => id}))
+    end
+    stylists
+  end
+
+
+
+
+end
---
+++ Desktop/hair_salon/spec/stylist_spec.rb
@@ -0,0 +1,46 @@
+require('spec_helper')
+
+describe(Stylist) do
+
+  describe('#name') do
+    it('returns the name of the stylist') do
+      test_stylist = Stylist.new({:name => "Fifi", :id => nil})
+      expect(test_stylist.name()).to(eq("Fifi"))
+    end
+  end
+
+  describe('#id') do
+    it('returns the id of the stylist') do
+      test_stylist = Stylist.new({:name => "Fifi", :id => nil})
+      expect(test_stylist.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe ('.all') do
+    it('will return empty at first') do
+      expect(Stylist.all()).to(eq([]))
+    end
+  end
+
+  describe('.find') do
+    it('will return a stylist by its id number') do
+      test_stylist = Stylist.new({:name => 'Fifi', :id => nil})
+      test_stylist.save()
+      test_stylist2 = Stylist.new({:name => 'Pierre', :id => nil})
+      test_stylist2.save()
+      expect(Stylist.find(test_stylist2.id())).to(eq(test_stylist2))
+    end
+  end
+
+  describe('#save') do
+    it('saves a stylist to the stylists table') do
+      test_stylist = Stylist.new({:name => 'Fifi', :id => nil})
+      test_stylist.save()
+      expect(Stylist.all()).to(eq([test_stylist]))
+    end
+  end
+
+
+
+
+end
